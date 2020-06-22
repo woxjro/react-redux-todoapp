0d2e26d5e85874334603fc3ed7718c003e4389ac
woxjro <n0i6c0k2@gmail.com>
2020-06-23 00:31:34 +0900

    first commit

---
+++ src/components/VisibilityFilters.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const VisibilityFilters = () => {
+    return (
+        <div>
+            <span>all</span>
+            <span>completed</span>
+            <span>incomplete</span>
+        </div>
+    );
+};
+
+export default VisibilityFilters;
